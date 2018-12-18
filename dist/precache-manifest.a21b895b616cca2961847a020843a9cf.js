2567dd9f74869f12a71f1167233d6d6f2a35373f
hxli8 <hxli8@iflytek.com>
2018-12-18 14:10:14 +0800

    new rc

---
+++ dist/precache-manifest.a21b895b616cca2961847a020843a9cf.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "92acc6829cf612e62ecf2be54e9344f0",
+    "url": "/admin-seed/fonts/font_148784_v4ggb6wrjmkotj4i.92acc682.ttf"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/admin-seed/robots.txt"
+  },
+  {
+    "revision": "f3572d89e4b28ee4b2d49d560a95e7eb",
+    "url": "/admin-seed/img/500.f3572d89.png"
+  },
+  {
+    "revision": "099dcc36d6b9bc4532d1",
+    "url": "/admin-seed/css/chunk-vendors.77f64a73.css"
+  },
+  {
+    "revision": "e62e952ee01ebb08edf1",
+    "url": "/admin-seed/css/login.ed8c8c66.css"
+  },
+  {
+    "revision": "e62e952ee01ebb08edf1",
+    "url": "/admin-seed/js/login.1a540d0c.js"
+  },
+  {
+    "revision": "1b35b521c0082d56b9ac413ddb0660ef",
+    "url": "/admin-seed/fonts/font_148784_v4ggb6wrjmkotj4i.1b35b521.woff"
+  },
+  {
+    "revision": "62759b1cf27452e085eddbb8ab29ad2b",
+    "url": "/admin-seed/fonts/font_148784_v4ggb6wrjmkotj4i.62759b1c.eot"
+  },
+  {
+    "revision": "3a2bd46b7b35bed7a5a1",
+    "url": "/admin-seed/js/app.196b08b7.js"
+  },
+  {
+    "revision": "099dcc36d6b9bc4532d1",
+    "url": "/admin-seed/js/chunk-vendors.fa5158c6.js"
+  },
+  {
+    "revision": "71a5fc392f50d54ebfb23c7e85458a37",
+    "url": "/admin-seed/img/404.71a5fc39.png"
+  },
+  {
+    "revision": "e5bfd0a8109a578756959a4907499d95",
+    "url": "/admin-seed/img/403.e5bfd0a8.png"
+  },
+  {
+    "revision": "725c4a1d46694ab5867255da7cbb5d82",
+    "url": "/admin-seed/img/font_148784_v4ggb6wrjmkotj4i.725c4a1d.svg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/admin-seed/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "9984c124fac95b889aae223f678a4b9e",
+    "url": "/admin-seed/index.html"
+  },
+  {
+    "revision": "7399a0dd6ac0e1c982c227c2a6d40dcb",
+    "url": "/admin-seed/login.html"
+  },
+  {
+    "revision": "3a2bd46b7b35bed7a5a1",
+    "url": "/admin-seed/css/app.6546de54.css"
+  }
+];
--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/admin-seed/precache-manifest.091f7717b646620225eb85fbc33b3ada.js"
+  "/admin-seed/precache-manifest.a21b895b616cca2961847a020843a9cf.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "seed-project"});
