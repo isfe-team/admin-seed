093f4bfa75f663a8012052dacd0a5bf10a12452b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-23 03:42:22 +0000

    Deploy isfe-team/admin-seed to github.com/isfe-team/admin-seed.git:gh-pages

--- precache-manifest.92d19bd2c422c57475c40ca8b98f05aa.js
+++ precache-manifest.92d19bd2c422c57475c40ca8b98f05aa.js
@@ -12,32 +12,32 @@ self.__precacheManifest = [
     "url": "/admin-seed/js/overview.aaa97ca5.js"
   },
   {
-    "revision": "a32fd2177e6e615d054d",
+    "revision": "16dd6ee0f83d39284a08",
     "url": "/admin-seed/css/chunk-vendors.233bad45.css"
   },
   {
-    "revision": "fb49cfd5b1fd02df3b09",
+    "revision": "c202fc76032a0e265fb6",
     "url": "/admin-seed/css/function_components.b8602b95.css"
   },
   {
-    "revision": "fb49cfd5b1fd02df3b09",
-    "url": "/admin-seed/js/function_components.f61784a9.js"
+    "revision": "c202fc76032a0e265fb6",
+    "url": "/admin-seed/js/function_components.950e0df8.js"
   },
   {
-    "revision": "99e2d46455fdc4623557",
+    "revision": "509ed787e9a000bfaf90",
     "url": "/admin-seed/css/login.d69d5e6e.css"
   },
   {
-    "revision": "99e2d46455fdc4623557",
-    "url": "/admin-seed/js/login.f6d691d6.js"
+    "revision": "509ed787e9a000bfaf90",
+    "url": "/admin-seed/js/login.8845cc84.js"
   },
   {
-    "revision": "870b5620100e4dbc84d3",
-    "url": "/admin-seed/js/app.3e100acc.js"
+    "revision": "eaedb4b0fae22f797635",
+    "url": "/admin-seed/js/app.62649dff.js"
   },
   {
-    "revision": "a32fd2177e6e615d054d",
-    "url": "/admin-seed/js/chunk-vendors.324cd4a7.js"
+    "revision": "16dd6ee0f83d39284a08",
+    "url": "/admin-seed/js/chunk-vendors.64e50902.js"
   },
   {
     "revision": "f3572d89e4b28ee4b2d49d560a95e7eb",
@@ -56,15 +56,15 @@ self.__precacheManifest = [
     "url": "/admin-seed/img/logo.82b9c7a5.png"
   },
   {
-    "revision": "f64ef7eaf1fc11ab8c7ca76ec2f07a5b",
+    "revision": "0929aa3f353fab84856d3a8014c70aae",
     "url": "/admin-seed/index.html"
   },
   {
-    "revision": "c8b05844a11c836f845899f4b7f7d4d2",
+    "revision": "59c658a9c02ccd41a738ec1cd1d5a900",
     "url": "/admin-seed/login.html"
   },
   {
-    "revision": "870b5620100e4dbc84d3",
+    "revision": "eaedb4b0fae22f797635",
     "url": "/admin-seed/css/app.6569316f.css"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/admin-seed/precache-manifest.c05c2e64498bce9f9fd0ace42ddef18d.js"
+  "/admin-seed/precache-manifest.92d19bd2c422c57475c40ca8b98f05aa.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "admin-seed"});
