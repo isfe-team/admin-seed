8e0cc550a0d903b4cad1452849a633adaa163173
lihaixiaad@163.com <x18755977004>
2021-06-09 18:11:35 +0800

    build: new rc

---
+++ precache-manifest.655d0e0f91b222452c9ace8fa3ae1e63.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "d3280d08d92973deb0e4",
+    "url": "admin-seed/js/main.280ca0cc.js"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "admin-seed/robots.txt"
+  },
+  {
+    "revision": "9b61565e81102a649dfc",
+    "url": "admin-seed/css/overview.39968b17.css"
+  },
+  {
+    "revision": "aeef51df902d719ee9c3",
+    "url": "admin-seed/js/antd-locale-zh_CN.df4d795a.js"
+  },
+  {
+    "revision": "233af90285076b89d26e",
+    "url": "admin-seed/js/app.f35bfec7.js"
+  },
+  {
+    "revision": "57fb70d4850cfb84fd5bb2c07bb55f58",
+    "url": "admin-seed/menu.json"
+  },
+  {
+    "revision": "dc3b2d6e72c0d9d03e99",
+    "url": "admin-seed/js/chunk-vendors.1f47b0b3.js"
+  },
+  {
+    "revision": "dae3871fc4dde1cbd85eb90c00d49fd6",
+    "url": "admin-seed/lang.csv"
+  },
+  {
+    "revision": "53a02d897c28e217678f",
+    "url": "admin-seed/js/function_components.75cd9f4a.js"
+  },
+  {
+    "revision": "9b61565e81102a649dfc",
+    "url": "admin-seed/js/overview.8b50bca3.js"
+  },
+  {
+    "revision": "8f5e9ec1efb3fb937582",
+    "url": "admin-seed/js/login.01c1411a.js"
+  },
+  {
+    "revision": "e5bfd0a8109a578756959a4907499d95",
+    "url": "admin-seed/img/403.e5bfd0a8.png"
+  },
+  {
+    "revision": "db42f838105de496c5e9",
+    "url": "admin-seed/js/antd-locale-en_US.f6449629.js"
+  },
+  {
+    "revision": "4088b9ee822a1e66433d",
+    "url": "admin-seed/js/antd-locale-ja_JP.380d97cf.js"
+  },
+  {
+    "revision": "12aaddc9d6c6583e3d6e95356bbad2f9",
+    "url": "admin-seed/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "admin-seed/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "f3572d89e4b28ee4b2d49d560a95e7eb",
+    "url": "admin-seed/img/500.f3572d89.png"
+  },
+  {
+    "revision": "71a5fc392f50d54ebfb23c7e85458a37",
+    "url": "admin-seed/img/404.71a5fc39.png"
+  },
+  {
+    "revision": "d3280d08d92973deb0e4",
+    "url": "admin-seed/css/main.0556f2e9.css"
+  },
+  {
+    "revision": "8f5e9ec1efb3fb937582",
+    "url": "admin-seed/css/login.bb6ccc82.css"
+  },
+  {
+    "revision": "53a02d897c28e217678f",
+    "url": "admin-seed/css/function_components.2dab45ec.css"
+  },
+  {
+    "revision": "dc3b2d6e72c0d9d03e99",
+    "url": "admin-seed/css/chunk-vendors.233bad45.css"
+  },
+  {
+    "revision": "233af90285076b89d26e",
+    "url": "admin-seed/css/app.9e7cc112.css"
+  },
+  {
+    "revision": "fc18504ae8885b8988d7a036f7d75b68",
+    "url": "admin-seed/config.js"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/admin-seed/precache-manifest.32a704153066e6789dfab62b94e4cec0.js"
+  "admin-seed/precache-manifest.655d0e0f91b222452c9ace8fa3ae1e63.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "admin-seed"});
